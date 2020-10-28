06ec3258c2d0b4c5f47e549c3860511f96eb37b6
Leonardo Gomide <leocgomide@gmail.com>
2020-10-28 11:32:31 -0300

    updating page using js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,122 @@
-function replaceSpotlightVideo(data) {
+var playlistPage = 1;
+var playlistPageSize = 2;
+var videoPageSize = 2;
+
+function updateSpotlightVideo(data) {
     let video = data[0].video;
     let spotlight = document.getElementById("spotlight");
 
     spotlight.getElementsByTagName("iframe")[0].src = video.url;
     spotlight.getElementsByClassName("title")[0].innerHTML = video.title;
-    spotlight.getElementsByClassName("description")[0].innerHTML = video.description;    
+    spotlight.getElementsByClassName("description")[0].innerHTML =
+        video.description;
+}
+
+function clearPlaylistVideos(playlist) {
+    let playlistDiv = document.getElementById("playlist-" + playlist);
+
+    let videoDivs = playlistDiv.getElementsByClassName("video-box");
+
+    for (let i = 0; i < videoDivs.length; i++) {
+        if (i == 0) {
+            videoDivs[i].id = "";
+            videoDivs[i].getElementsByTagName("iframe")[0].src = "";
+            videoDivs[i].getElementsByClassName("title")[0].innerHTML = "";
+            videoDivs[i].getElementsByClassName("description")[0].innerHTML =
+                "";
+        } else {
+            videoDivs[i].remove();
+        }
+    }
+}
+
+function updatePlaylistVideos(data) {
+    if (data.length) {
+        let playlistDiv = document
+            .getElementById("playlist-" + data[0].playlistId)
+            .getElementsByClassName("videos-row")[0];
+        let videoDiv = playlistDiv.getElementsByClassName("video-box")[0];
+
+        data.forEach((video) => {
+            let thisVideoDiv;
+
+            if (videoDiv.id) {
+                thisVideoDiv = videoDiv.cloneNode(true);
+                playlistDiv.appendChild(thisVideoDiv);
+            } else {
+                thisVideoDiv = videoDiv;
+            }
+
+            thisVideoDiv.id = "video-" + video.id;
+            thisVideoDiv.getElementsByTagName("iframe")[0].src = video.url;
+            thisVideoDiv.getElementsByClassName("title")[0].innerHTML =
+                video.title;
+            thisVideoDiv.getElementsByClassName("description")[0].innerHTML =
+                video.description;
+        });
+    }
+}
+
+function loadMoreVideos(page, playlist) {
+    let playlistDiv = document.getElementById("playlist-" + playlist);
+
+    getVideosByPlaylist(page, videoPageSize, playlist, updatePlaylistVideos);
+
+    playlistDiv.getElementsByClassName(
+        "btn-load-more"
+    )[0].onclick = function () {
+        loadMoreVideos(page + 1, playlist);
+    };
+}
+
+function updatePlaylists(data) {
+    let videosDiv = document.getElementsByClassName("videos")[0];
+    let videosSection = document.getElementsByClassName("videos-section")[0];
+    data.forEach((playlist) => {
+        let thisVideosDiv;
+
+        if (videosDiv.id) {
+            thisVideosDiv = videosDiv.cloneNode(true);
+            videosSection.appendChild(thisVideosDiv);
+        } else {
+            thisVideosDiv = videosDiv;
+        }
+
+        thisVideosDiv.id = "playlist-" + playlist.id;
+        thisVideosDiv.getElementsByClassName("title")[0].innerHTML =
+            playlist.name;
+
+        clearPlaylistVideos(playlist.id);
+
+        getVideosByPlaylist(
+            1,
+            videoPageSize,
+            playlist.id,
+            updatePlaylistVideos
+        );
+
+        thisVideosDiv.getElementsByClassName(
+            "btn-load-more"
+        )[0].onclick = function () {
+            loadMoreVideos(2, playlist.id);
+        };
+    });
 }
 
 function start() {
-    getSpotlightVideo("home", replaceSpotlightVideo);
+    getSpotlightVideo("home", updateSpotlightVideo);
+    getPlaylistByPage(playlistPage, playlistPageSize, updatePlaylists);
+    playlistPage++;
 }
 
 $(document).ready(start);
+
+$(window).scroll(function () {
+    if (
+        $(window).scrollTop() + $(window).height() >
+        $(document).height() - 150
+    ) {
+        getPlaylistByPage(playlistPage, playlistPageSize, updatePlaylists);
+        playlistPage++;
+    }
+});
--- src/scripts/proxy.js
+++ src/scripts/proxy.js
@@ -1,7 +1,7 @@
 // local
-// const apiUrl = "http://localhost:3000/";
+const apiUrl = "http://localhost:3000/";
 // heroku
-const apiUrl = "https://libera-libras-api.herokuapp.com/";
+//const apiUrl = "https://libera-libras-api.herokuapp.com/";
 
 function htmlGetRequest(url, callback) {
     var request = new XMLHttpRequest();
@@ -18,7 +18,35 @@ function htmlGetRequest(url, callback) {
 }
 
 function getSpotlightVideo(screen, callback) {
-    let url = "spotlightVideos?_expand=video&screen=" + screen; 
-    
+    let url = "spotlightVideos?_expand=video&screen=" + screen;
+
+    htmlGetRequest(url, callback);
+}
+
+function getPlaylistById(id, callback) {
+    let url = "playlists/" + id + "/_embed=videos";
+
+    htmlGetRequest(url, callback);
+}
+
+function getPlaylistByPage(page, pageSize, callback) {
+    let url =
+        "playlists?order_gte=1&_sort=order&_order=asc&_nested=videos&_page=" +
+        page +
+        "&_limit=" +
+        pageSize;
+
+    htmlGetRequest(url, callback);
+}
+
+function getVideosByPlaylist(page, pageSize, playlist, callback) {
+    let url =
+        "videos?_sort=id&_order=asc&playlistId=" +
+        playlist +
+        "&_page=" +
+        page +
+        "&_limit=" +
+        pageSize;
+
     htmlGetRequest(url, callback);
 }

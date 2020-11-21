c670f66a0c9a3025a2d774f420a9a530c666c35c
Layla Raissa silva Pereira <raissalayla16@gmail.com>
2020-11-21 13:27:59 -0300

    Comentários

--- src/screens/news/app.js
+++ src/screens/news/app.js
@@ -51,3 +51,56 @@ function start() {
 }
 
 $(document).ready(start);
+/*Cometários js*/
+function leDados( ) {
+    let strDados = localStorage.getItem('db');
+     let objDados = {};
+    if(strDados) {
+       objDados = JSON.parse(strDados);
+    }
+   else {
+       objDados = {Comentário:[
+                         {Nome: "Joana Silva", Comentário:"legal"},
+                         {Nome: "Diana Vilela", Comentário:"gostei"}
+                        ] }
+   }
+    return objDados;
+}
+
+function salvaDados (dados) {
+   localStorage.setItem('db',JSON.stringify (dados));
+}
+
+function incluirComentarios (){
+   //Ler os dados do localStorage
+   let objDados = leDados();
+
+   //Incluir um novo contato
+   let strNome = document.getElementById('campoNome').value;
+   let strComentario = document.getElementById('cMsg').value;
+   let novoComentario = {
+       Nome: strNome,
+       Comentário: strComentario
+   };
+   objDados.Comentário.push (novoComentario);
+
+   //Salvar os dados no localStorage novamente 
+    salvaDados (objDados);
+
+    //Atualiza os dados  da tela 
+    imprimeDados();
+}
+ 
+
+function imprimeDados (dados) {
+    let tela = document.getElementById('tela');
+    let strHtml = '';
+    let objDados = leDados ();
+    for (i=0; i< objDados.Comentário.length; i++) {
+        strHtml += `<p>${objDados.Comentário[i].Nome}-${objDados.Comentário[i].Comentário}</p>`
+    }
+    tela.innerHTML = strHtml;
+}
+//configura os botões
+document.getElementById('btnCarregaDados').addEventListener('click',imprimeDados);
+document.getElementById('btnIncluirComentarios').addEventListener('click',incluirComentarios);

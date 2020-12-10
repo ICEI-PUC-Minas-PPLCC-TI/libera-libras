caaf4ccf81661e89f44e78b425b4019e5c16ad7f
Leonardo Gomide <leocgomide@gmail.com>
2020-12-10 09:28:54 -0300

    sugestões no jsonserver

--- src/screens/sobre/index.js
+++ src/screens/sobre/index.js
@@ -3,30 +3,20 @@ function newsFeedBox(title, desc) {
     this.desc = desc;
 }
 
-let vagas = [];
+let suggestions = [];
 
+function fillSugestions(data) {
+    suggestions = data;
 
-//Garantir que hajam vagas ficticias postadas 
-
-if (localStorage.getItem('Vagas') === null) {
-    localStorage.setItem('Vagas', `[{"title":"Lucas Magalhaes","desc":"Otima interface!"},{"title":"Luca Fonseca","desc":"Muito boa ideia! Sofro muito por ser mudo e ninguem saber conversar por libras."},{"title":"Luisa Alcantra","desc":"Muito boa ideia, o estudo de libras deveria ser uma materia na escola!"}]`);
-} else {
-    console.log("local storage preenchido");
-}
-
-vagas = JSON.parse(localStorage.getItem("Vagas"));
-
-window.onload = () => {
-
-    for (x = vagas.length - 1; x >= 0; x--) {
-        let newsFeed = document.getElementById('newsFeed');
+    for (x = suggestions.length - 1; x >= 0; x--) {
+        let newsFeed = document.getElementById("newsFeed");
 
         let box = `<div class="card mb-3 col-12">
             <div class="row no-gutters">
             <div class="col-md-8">
             <div class="card-body">
-            <h5 class="card-title">${vagas[x].title}</h5>
-            <p class="card-text">${vagas[x].desc}
+            <h5 class="card-title">${suggestions[x].title}</h5>
+            <p class="card-text">${suggestions[x].desc}
             </p>
             </div>
             </div>
@@ -35,6 +25,14 @@ window.onload = () => {
 
         newsFeed.innerHTML += box;
     }
+}
+
+
+
+
+window.onload = () => {
+
+    getSuggestions(fillSugestions);
 
     register.onsubmit = (e) => {
 
@@ -42,17 +40,17 @@ window.onload = () => {
             descForm.value.length == 0) {
 
             instrucoes.classList.add("erro");
-            instrucoes.innerHTML = "Prença todos os campos";
+            instrucoes.innerHTML = "Preencha todos os campos";
             console.log("erro");
         } else {
 
-            let info = new newsFeedBox(titleForm.value, descForm.value,);
+            let info = new newsFeedBox(titleForm.value, descForm.value);
 
-            vagas[vagas.length] = info;
+            suggestions[suggestions.length] = info;
             instrucoes.innerHTML = "";
             instrucoes.classList.remove("erro");
 
-            localStorage.setItem('Vagas', JSON.stringify(vagas));
+            localStorage.setItem('suggestions', JSON.stringify(suggestions));
 
             newsFeed = document.getElementById('newsFeed');
 
@@ -60,8 +58,8 @@ window.onload = () => {
                                 <div class="row no-gutters">
                                     <div class="col-md-8">
                                     <div class="card-body">
-                                            <h5 class="card-title">${vagas[vagas.length - 1].title}</h5>
-                                            <p class="card-text">${vagas[vagas.length - 1].desc}
+                                            <h5 class="card-title">${suggestions[suggestions.length - 1].title}</h5>
+                                            <p class="card-text">${suggestions[suggestions.length - 1].desc}
                                             </p>
                                             </div>
                                 </div>
